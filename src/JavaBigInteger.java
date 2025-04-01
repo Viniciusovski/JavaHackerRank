b44e6d38f46abe07943069a40eb1179c58f59f2f
Viniciusovski <vini98.2012@gmail.com>
2025-04-01 11:11:15 -0300

    JavaBigInteger

---
+++ src/JavaBigInteger.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class JavaBigInteger {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String num1Str = scanner.nextLine();
+        String num2Str = scanner.nextLine();
+
+        BigInteger num1 = new BigInteger(num1Str);
+        BigInteger num2 = new BigInteger(num2Str);
+
+        BigInteger sum = num1.add(num2);
+        BigInteger product = num1.multiply(num2);
+
+        System.out.println(sum);
+        System.out.println(product);
+    }
+}
