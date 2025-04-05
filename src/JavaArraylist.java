f6ca32ccc69af6139693776ee222ca164536c136
Viniciusovski <vini98.2012@gmail.com>
2025-04-05 17:11:27 -0300

    JavaArraylist

---
+++ src/JavaArraylist.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaArraylist {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<ArrayList<Integer>> lines = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int d = scanner.nextInt();
+            ArrayList<Integer> line = new ArrayList<>();
+            for (int j = 0; j < d; j++) {
+                line.add(scanner.nextInt());
+            }
+            lines.add(line);
+        }
+
+        int q = scanner.nextInt();
+        for (int i = 0; i < q; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            if (x <= lines.size() && x > 0) {
+                ArrayList<Integer> line = lines.get(x - 1);
+                if (y <= line.size() && y > 0) {
+                    System.out.println(line.get(y - 1));
+                } else {
+                    System.out.println("ERROR!");
+                }
+            } else {
+                System.out.println("ERROR!");
+            }
+        }
+        scanner.close();
+    }
+}
