22f42c87d46ba62c73d6ca5f04d2b33dcc3ecd44
Viniciusovski <vini98.2012@gmail.com>
2025-04-07 14:04:46 -0300

    JavaList

---
+++ src/JavaList.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class JavaList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(scanner.nextInt());
+        }
+        int q = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < q; i++) {
+            String operation = scanner.nextLine();
+            if (operation.equals("Insert")) {
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+                list.add(x, y);
+                if (scanner.hasNextLine()) {
+                    scanner.nextLine();
+                }
+            } else if (operation.equals("Delete")) {
+                int x = scanner.nextInt();
+                list.remove(x);
+                if (scanner.hasNextLine()) {
+                    scanner.nextLine();
+                }
+            }
+        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i));
+            if (i < list.size() - 1) {
+                System.out.print(" ");
+            }
+        }
+        scanner.close();
+    }
+}
