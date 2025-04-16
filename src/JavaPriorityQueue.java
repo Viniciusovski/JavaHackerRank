8fb7ee52e19177f907dcfb24cc0ec2fd4e236f2a
Viniciusovski <vini98.2012@gmail.com>
2025-04-16 20:20:01 -0300

    JavaPriorityQueue

---
+++ src/JavaPriorityQueue.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class Student implements Comparable<Student> {
+    private int id;
+    private String name;
+    private double cgpa;
+
+    public Student(int id, String name, double cgpa) {
+        this.id = id;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCGPA() {
+        return cgpa;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        if (this.cgpa != other.cgpa) {
+            return Double.compare(other.cgpa, this.cgpa); // Higher CGPA first
+        } else if (!this.name.equals(other.name)) {
+            return this.name.compareTo(other.name); // Ascending alphabetical order
+        } else {
+            return Integer.compare(this.id, other.id); // Ascending ID order
+        }
+    }
+}
+
+class Priorities {
+    public List<Student> getStudents(List<String> events) {
+        PriorityQueue<Student> priorityQueue = new PriorityQueue<>();
+
+        for (String event : events) {
+            String[] parts = event.split(" ");
+            if (parts[0].equals("ENTER")) {
+                String name = parts[1];
+                double cgpa = Double.parseDouble(parts[2]);
+                int id = Integer.parseInt(parts[3]);
+                Student student = new Student(id, name, cgpa);
+                priorityQueue.add(student);
+            } else if (parts[0].equals("SERVED")) {
+                priorityQueue.poll();
+            }
+        }
+
+        List<Student> students = new ArrayList<>();
+        while (!priorityQueue.isEmpty()) {
+            students.add(priorityQueue.poll());
+        }
+        return students;
+    }
+}
+
+public class JavaPriorityQueue {
+    private final static Scanner scan = new Scanner(System.in);
+    private final static Priorities priorities = new Priorities();
+
+    public static void main(String[] args) {
+        int totalEvents = Integer.parseInt(scan.nextLine());
+        List<String> events = new ArrayList<>();
+
+        while (totalEvents-- != 0) {
+            String event = scan.nextLine();
+            events.add(event);
+        }
+
+        List<Student> students = priorities.getStudents(events);
+
+        if (students.isEmpty()) {
+            System.out.println("EMPTY");
+        } else {
+            for (Student st: students) {
+                System.out.println(st.getName());
+            }
+        }
+    }
+}
