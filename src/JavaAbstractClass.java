4072a9101d6ff5de4e3235f5eeca8570c4c664fb
Viniciusovski <vini98.2012@gmail.com>
2025-04-22 19:17:10 -0300

    JavaAbstractClass

---
+++ src/JavaAbstractClass.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+abstract class Book {
+    String title;
+    abstract void setTitle(String s);
+    String getTitle() {
+        return title;
+    }
+}
+
+// Write your code here
+class MyBook extends Book {
+    void setTitle(String s) {
+        this.title = s;
+    }
+}
+
+public class JavaAbstractClass {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        MyBook new_novel = new MyBook();
+        new_novel.setTitle(title);
+        System.out.println("The title is: " + new_novel.getTitle());
+    }
+}
